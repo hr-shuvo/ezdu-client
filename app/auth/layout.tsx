3c6c86c7df4e2ec842f61b4b312a8126f5577473
hr-shuvo <h.r.shuvo9@gmail.com>
2025-02-10 19:18:23 +0600

    fix

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,6 +1,9 @@
+type Props = {
+    children: React.ReactNode
+}
 
 
-const AuthLayout = ({children}: React.ReactNode) => {
+const AuthLayout = ({children}: Props) => {
     return (
         <div className='h-full flex items-center justify-center bg-red-10'>
             {children}
