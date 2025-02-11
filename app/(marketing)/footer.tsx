5416717464e60f88c0126263d5ca2929efe82289
hr-shuvo <h.r.shuvo9@gmail.com>
2025-02-11 07:55:12 +0600

    add footer

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,34 @@
+import { Button } from "@/components/ui/button";
+// import Image from "next/image";
+
 export const Footer =() =>{
     return(
         <footer className='hidden md:block h-20 w-full border-t-2 border-slate-200 p-2'>
-            Footer
+            <div className='max-w-screen-lg mx-auto flex items-center justify-evenly h-full'>
+                <Button size='lg' variant='ghost' className='w-full'>
+                    {/* <Image
+                        src='/logo/logo_bgw.gif'
+                        alt='Croatian'
+                        height={32}
+                        width={40}
+                        className='mr-4 rounded-md'
+                    /> */}
+                    <span className='font-bold font-serif'>English Proficiency</span>
+                </Button>
+
+                <Button size='lg' variant='ghost' className='w-full'>
+                    <span className='font-bold font-serif'>Quantitive Tests</span>
+                </Button>
+
+                <Button size='lg' variant='ghost' className='w-full'>
+                    <span className='font-bold font-serif'>IELTS</span>
+                </Button>
+
+                <Button size='lg' variant='ghost' className='w-full'>
+                    <span className='font-bold font-serif'>GRE</span>
+                </Button>
+
+            </div>
 
         </footer>
     )
