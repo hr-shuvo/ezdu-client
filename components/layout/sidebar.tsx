914e097ef0235d021c7d9082602d7b8c42c3f02a
hr-shuvo <h.r.shuvo9@gmail.com>
2025-05-19 03:21:28 +0600

    update

--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -14,7 +14,7 @@ export const Sidebar = ({className}: Props) =>{
         <div className={cn('flex h-full lg:w-[256px] w-[256px] ' +
             'lg:fixed left-0 top-0 px-4 border-r-2 flex-col', className)}>
 
-            <Link href='/learn'>
+            <Link href='/'>
                 <div className='pt-8 pl-4 pb-7 flex items-center gap-x-3'>
                     <Image src='/logo/logo.png' height={40} width={40} alt='logo'/>
                     <h1 className='text-2xl font-extrabold text-sky-400 tracking-wide'>
@@ -27,8 +27,8 @@ export const Sidebar = ({className}: Props) =>{
 
             <div className='flex flex-col gap-y-2 flex-1'>
                 <SidebarItem label='Learn' href='/learn' iconSrc='/common/learn.svg'/>
-                <SidebarItem label='Leaderboard' href='/leaderboard' iconSrc='/common/leaderboard.svg'/>
-                <SidebarItem label='Quests' href='/quests' iconSrc='/common/quests.svg'/>
+                {/*<SidebarItem label='Leaderboard' href='/leaderboard' iconSrc='/common/leaderboard.svg'/>*/}
+                {/*<SidebarItem label='Quests' href='/quests' iconSrc='/common/quests.svg'/>*/}
                 <SidebarItem label='Shop' href='/shop' iconSrc='/common/shop.svg'/>
 
             </div>
