1c6ab4799cf4664395b5aa16e1dfb9e4bb2b3f19
hr-shuvo <h.r.shuvo9@gmail.com>
2025-05-19 05:43:36 +0600

    fix

--- components/sections/footer/default.tsx
+++ components/sections/footer/default.tsx
@@ -41,7 +41,7 @@ export default function FooterSection() {
 
             <FooterColumn>
               <h2 className="text-md pt-1 font-semibold">Company</h2>
-              <Link href="/about" className="text-sm text-muted-foreground"> About</Link>
+              <Link href="https://www.facebook.com/profile.php?id=61576033716497&sk=about" className="text-sm text-muted-foreground"> About</Link>
               <Link href="/" className="text-sm text-muted-foreground">Careers</Link>
               <Link href="/" className="text-sm text-muted-foreground">Blog</Link>
             </FooterColumn>
@@ -50,7 +50,7 @@ export default function FooterSection() {
               <h2 className="text-md pt-1 font-semibold">Contact</h2>
               {/*<Link href='https://discord.gg/kRZa3WmwSM' target="_blank" className="text-sm text-muted-foreground ">Discord</Link>*/}
               <Link href="#" className="text-sm text-muted-foreground">Twitter</Link>
-              <Link href="#" className="text-sm text-muted-foreground">Facebook</Link>
+              <Link href="https://www.facebook.com/share/19MNpPPQMh/?mibextid=wwXIfr" className="text-sm text-muted-foreground">Facebook</Link>
             </FooterColumn>
 
 
@@ -58,7 +58,7 @@ export default function FooterSection() {
 
 
           <FooterBottom>
-            <div>© 2025 Mikołaj Dobrucki. All rights reserved</div>
+            <div>© 2025 ezduonline.com - All rights reserved</div>
             <div className="flex items-center gap-4">
               <Link href="/policy">Privacy Policy</Link>
               <Link href="/terms">Terms of Service</Link>
