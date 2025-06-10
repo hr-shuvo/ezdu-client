c2cde782b6450a5448800f10078292932b39470e
hr-shuvo <h.r.shuvo9@gmail.com>
2025-06-10 20:47:53 +0600

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,50 @@
+import { MetadataRoute } from "next";
+
+
+interface Subject {
+    _id: string;
+}
+
+interface ApiResponse {
+    data: Subject[];
+    total?: number;
+    page?: number;
+    pageSize?: number;
+}
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+    const baseUrl = 'https://www.ezduonline.com';
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+    
+    const responseData: ApiResponse = await (await fetch(`${apiUrl}academy/subjects?pg=1&sz=100`)).json();
+    const subjects: Subject[] = responseData.data || [];
+
+    const subjectSitemapEntries = subjects.map((subject: Subject) => ({
+        url: `${baseUrl}/academy/subjects/${subject._id}`, 
+        changeFrequency: 'monthly' as 'monthly', 
+        priority: 0.6,
+    }));
+
+    return [
+        {
+            url: `${baseUrl}/academy`,
+            // lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 1.0,
+        },
+        {
+            url: `${baseUrl}/academy/quiz`,
+            // lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.9,
+        },
+        {
+            url: `${baseUrl}/academy/qb`,
+            // lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 0.9,
+        },
+        ...subjectSitemapEntries
+
+    ]
+}
