1a80a86bd29505e144ae7cf897d8ff68cd476dcf
hr-shuvo <h.r.shuvo9@gmail.com>
2025-06-17 01:02:00 +0600

    build

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -17,7 +17,7 @@ const AuthLayout = ({ children }: Props) => {
                 <div className="flex-1 flex items-center justify-around bg-gradient-to-r from-white via-indigo-100 to-white">
                     <div>
                         <div className="relative w-[280px] h-[280px] lg:w-[380px] lg:h-[380px] mb-8 lg:mb-0">
-                            <Image src="/images/marketing/quiz.gif" alt="Ezdu quiz Logo" fill/>
+                            <Image src="/logo/logo_bgw.gif" alt="Ezdu quiz Logo" fill/>
                         </div>
                     </div>
                     <div>{children}</div>
